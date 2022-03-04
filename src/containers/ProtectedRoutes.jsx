2ca3385dcd5e446a1ca1a423d02280b0fb42937c
jin park <jinpark0625@gmail.com>
2022-03-05 03:44:31 +0900

    fix error

--- src/containers/ProtectedRoutes.jsx
+++ src/containers/ProtectedRoutes.jsx
@@ -6,9 +6,8 @@ import { Route } from "react-router-dom";
 const ProtectedRoutes = inject("authStore")(
   observer(({ authStore, authenticated }) => {
     const auth = authStore.authenticated;
-    console.log("protected 1번", authenticated);
-    return authenticated ? <Outlet /> : <Navigate to="/" />;
+    return auth || authenticated ? <Outlet /> : <Navigate to="/" />;
   })
 );
-// authenticated ? <Outlet /> : <Navigate to="/" />;
+
 export default ProtectedRoutes;
