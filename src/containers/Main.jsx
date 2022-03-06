35eb8dae0e6404e87f7788f501e111371dcf6d04
jin park <jinpark0625@gmail.com>
2022-03-07 01:40:13 +0900

    add userList API

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SideMenu, Header } from "../components";
 import { FlexBox } from "../components/StyledElements";
 import { inject, observer } from "mobx-react";
 import { Outlet } from "react-router-dom";
+import axios from "../utils/axios";
 
 const Main = inject("authStore")(
   observer(({ authStore }) => {
-    const sss = "hh";
+    console.log(authStore.accessToken);
+    //?page=1&page_size=10
+    const getUserList = async () => {
+      if (authStore.accessToken) {
+        try {
+          const response = await axios.get("/api/user?page=1&page_size=10", {
+            headers: {
+              Authorization: `Bearer ${authStore.accessToken}`,
+            },
+          });
+          console.log(response);
+          return response;
+        } catch (error) {
+          console.log(error.response);
+        }
+      }
+    };
+
+    useEffect(() => {
+      getUserList();
+    }, []);
+
+    const sss = "gg";
 
     return (
       <FlexBox width="100%" height="100%" background="#ededed">
