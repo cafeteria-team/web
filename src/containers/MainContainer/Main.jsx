3b844ae2fdc5b3010d0ccad16e24126d8d053d4f
jin park <jinpark0625@gmail.com>
2022-05-21 23:39:45 -0400

    [MODIFY]deleted previous states(username, userRole)

--- src/containers/MainContainer/Main.jsx
+++ src/containers/MainContainer/Main.jsx
@@ -1,24 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { SideMenu, Header } from "../../components";
 import { FlexBox } from "../../components/StyledElements";
 import { observer } from "mobx-react";
 import { Outlet } from "react-router-dom";
 import { useStores } from "../../stores/Context";
-import Decode from "../../utils/decode";
-import axios from "../../utils/axios";
-import moment from "moment";
 
 const Main = observer(() => {
   const { AuthStore } = useStores();
 
-  // 사용자 이름저장
-  const [userName, setUserName] = useState("");
-  const [userRole, setUserRole] = useState("");
-
+  // 렌더링 될시에 store 에 user 정보를 저장
   const callUser = () => {
     const access = localStorage.getItem("access");
     AuthStore.setUser(access);
-    console.log(AuthStore.getUser);
   };
 
   useEffect(() => {
@@ -27,13 +20,13 @@ const Main = observer(() => {
 
   return (
     <FlexBox width="100%" height="100%" minHeight="100vh" background="#F9FAFB">
-      {/* <SideMenu role={userRole}></SideMenu>
+      <SideMenu role={AuthStore.getUser.userRole}></SideMenu>
       <FlexBox direction="column" width="100%" height="100%">
-        <Header name={userName} logout={AuthStore.logout} />
+        <Header name={AuthStore.getUser.userName} logout={AuthStore.logout} />
         <FlexBox height="100%">
           <Outlet />
         </FlexBox>
-      </FlexBox> */}
+      </FlexBox>
     </FlexBox>
   );
 });
