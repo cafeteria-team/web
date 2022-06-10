b190a7f3527c7d3665078dda4b55283c8631700a
jin park <jinpark0625@gmail.com>
2022-06-10 13:16:56 -0400

    [MODIFY]fixed error in refrsh token

--- src/utils/refreshToken.js
+++ src/utils/refreshToken.js
@@ -1,5 +1,6 @@
 import { requestInstance } from "./axios";
 import Decode from "./decode";
+import { useNavigate } from "react-router-dom";
 
 const checkToken = async (config) => {
   const refreshToken = localStorage.getItem("refresh");
@@ -9,21 +10,27 @@ const checkToken = async (config) => {
 
   // 토큰이 만료되고, refreshToken이 저장되어 있을때
   if (currentTime >= expireAt && refreshToken) {
-    const { data } = await requestInstance.post("/api/user/token/refresh/", {
-      refresh: refreshToken,
-    });
+    try {
+      const { data } = await requestInstance.post("/api/user/token/refresh/", {
+        refresh: refreshToken,
+      });
+      const decode = new Decode();
+      const access = data.access;
+      const refresh = data.refresh;
+      const { exp } = decode.getUserId(access);
+      localStorage.setItem("access", access);
+      localStorage.setItem("refresh", refresh);
+      localStorage.setItem("expireAt", exp);
 
-    const decode = new Decode();
-    const access = data.access;
-    const refresh = data.refresh;
-    const { exp } = decode.getUserId(access);
-    localStorage.setItem("access", access);
-    localStorage.setItem("refresh", refresh);
-    localStorage.setItem("expireAt", exp);
+      config.headers["Authorization"] = `Bearer ${access}`;
 
-    config.headers["Authorization"] = `Bearer ${access}`;
-
-    return config;
+      return config;
+    } catch (err) {
+      alert("세션이 만료되었습니다. 다시 로그인 해주십시오.");
+      localStorage.clear();
+      window.location.reload();
+      return config;
+    }
   } else if (currentTime < expireAt && refreshToken) {
     config.headers["Authorization"] = `Bearer ${access}`;
   }
